abf88ab12f2bd1e190f55c303a027d8a04b76755
tomonari <zcsxcacg@i.softbank.jp>
2022-01-18 22:08:18 +0900

    クラス外から呼び出さないメソッドをPrivateに変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,6 +4,34 @@ require 'date'
 require 'optparse'
 
 class Calendar
+  def print_days
+    get_first_and_last_day
+    print("      #{@first_day.mon}月 #{@first_day.year}\n")
+    print(" 日 月 火 水 木 金 土\n")
+    @first_day.wday.times do
+      print('   ')
+    end
+    (@first_day..@last_day).each do |day|
+      print(' ')
+      @this_day = day.day
+      print(' ') if @this_day.between?(1, 9)
+      if is_today?
+        print("\e[7m#{@this_day}\e[0m")
+      else
+        print(@this_day)
+      end
+      print("\n") if day.saturday?
+    end
+  end
+
+  private
+
+  def get_first_and_last_day
+    get_option
+    @first_day = Date.new(@year, @month, 1)
+    @last_day = Date.new(@year, @month, -1)
+  end
+  
   def get_option
     options = ARGV.getopts('m:', 'y:')
     if options['y'].nil?
@@ -26,34 +54,8 @@ class Calendar
     end
   end
 
-  def get_first_and_last_day
-    get_option
-    @first_day = Date.new(@year, @month, 1)
-    @last_day = Date.new(@year, @month, -1)
-  end
-
   def is_today?
     @year == Date.today.year && @month == Date.today.mon && @this_day == Date.today.day
-  end
-
-  def print_days
-    get_first_and_last_day
-    print("      #{@first_day.mon}月 #{@first_day.year}\n")
-    print(" 日 月 火 水 木 金 土\n")
-    @first_day.wday.times do
-      print('   ')
-    end
-    (@first_day..@last_day).each do |day|
-      print(' ')
-      @this_day = day.day
-      print(' ') if @this_day.between?(1, 9)
-      if is_today?
-        print("\e[7m#{@this_day}\e[0m")
-      else
-        print(@this_day)
-      end
-      print("\n") if day.saturday?
-    end
   end
 end
 
