f050fa0f5bfc46b0ff20a98e7b9547b2c9610c7f
tomonari <zcsxcacg@i.softbank.jp>
2022-01-18 13:10:56 +0900

    指摘頂いた点を修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,55 +1,61 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
+
 class Calendar
   def get_option
-    options = ARGV.getopts('m:','y:')
-    if options["y"] == nil
+    options = ARGV.getopts('m:', 'y:')
+    if options['y'].nil?
       @year = Date.today.year
-    else @year = options["y"].to_i
-      unless @year.between?(1, 9999) == true
-        print("cal: year `#{options["y"]}' not in range 1..9999")
+    else
+      @year = options['y'].to_i
+      unless @year.between?(1, 9999)
+        print("cal: year `#{options['y']}' not in range 1..9999")
         exit
       end
     end
-    if options["m"] == nil
+    if options['m'].nil?
       @month = Date.today.mon
-    else @month = options["m"].to_i
-      unless @month.between?(1, 12) == true 
-        print("cal: #{options["m"]} is neither a month number(1..12) nor name")
+    else
+      @month = options['m'].to_i
+      unless @month.between?(1, 12)
+        print("cal: #{options['m']} is not a month number(1..12)")
         exit
-      end 
+      end
     end
   end
+
   def get_first_and_last_day
-    self.get_option  
+    get_option
     @first_day = Date.new(@year, @month, 1)
     @last_day = Date.new(@year, @month, -1)
   end
+
+  def check_wether_today?
+    @year == Date.today.year && @month == Date.today.mon && @this_day == Date.today.day
+  end
+
   def print_days
-    self.get_first_and_last_day
-    print("      " + @first_day.mon.to_s + "月 " + @first_day.year.to_s + "\n")
+    get_first_and_last_day
+    print("      #{@first_day.mon}月 #{@first_day.year}\n")
     print(" 日 月 火 水 木 金 土\n")
     @first_day.wday.times do
-      print("   ")
-    end 
+      print('   ')
+    end
     (@first_day..@last_day).each do |day|
-      print(" ")
-      if day.day < 10
-        print(" ")
+      print(' ')
+      @this_day = day.day
+      print(' ') if @this_day.between?(1, 9)
+      if check_wether_today?
+        print("\e[7m#{@this_day}\e[0m")
+      else
+        print(@this_day)
       end
-      if @year == Date.today.year && @month == Date.today.mon && day.day == Date.today.day
-        print("\e[7m#{day.day}\e[0m")
-          if day.saturday? == true
-            print("\n")
-          end
-        next
-      end
-      print(day.day)
-      if day.saturday? == true
-        print("\n")
-      end
+      print("\n") if day.saturday?
     end
   end
 end
+
 calendar = Calendar.new
 calendar.print_days
