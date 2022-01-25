b4c649826a0938d308f3a405c4dc02ff5c51e8b3
tomonari <zcsxcacg@i.softbank.jp>
2022-01-25 16:03:48 +0900

    rubocop対応

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,22 @@
-@COLUMN = 3
+# frozen_string_literal: true
+
+COLUMN = 3
 
 def print_list
-  get_list
-  row = @list.size / @COLUMN
-  row += 1 unless @list.size % @COLUMN == 0
-  (0..row-1).each do |x|
-    (0..@COLUMN-1).each do |number|
-      print @list[x + row * number].to_s.ljust(24)
+  generate_list
+  row = @list.size / COLUMN
+  row += 1 unless (@list.size % COLUMN).zero?
+  (0..row - 1).each do |x|
+    (0..COLUMN - 1).each do |y|
+      print @list[x + row * y].to_s.ljust(24)
     end
     print "\n"
   end
 end
 
-def get_list
+def generate_list
   @list = []
-  Dir.glob("*"){|f|@list << f}
+  Dir.glob('*') { |f| @list << f }
 end
 
 print_list
