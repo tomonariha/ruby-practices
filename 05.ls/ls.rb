563dbd804db864fc687939cc9913f46857751096
tomonari <zcsxcacg@i.softbank.jp>
2022-01-25 13:30:05 +0900

    メソッド化し、定数・変数の名前も変更した

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,11 +1,20 @@
-list = []
-Dir.glob("*"){|f|list << f}
-@LINE = 3
-@karam = list.size / @LINE
-@karam += 1 unless list.size % @LINE == 0
-(0..@karam-1).each do |x|
-  (0..@LINE-1).each do |number|
-    print list[x + @karam * number].to_s.ljust(24)
+@COLUMN = 3
+
+def print_list
+  get_list
+  row = @list.size / @COLUMN
+  row += 1 unless @list.size % @COLUMN == 0
+  (0..row-1).each do |x|
+    (0..@COLUMN-1).each do |number|
+      print @list[x + row * number].to_s.ljust(24)
+    end
+    print "\n"
   end
-  print "\n"
 end
+
+def get_list
+  @list = []
+  Dir.glob("*"){|f|@list << f}
+end
+
+print_list
