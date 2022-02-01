d13acc915c5976e4f32f67ab07e2f53ef38b510a
tomonari <zcsxcacg@i.softbank.jp>
2022-02-01 13:02:55 +0900

    範囲の書き方を...に修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -16,8 +16,8 @@ def print_list
   generate_list
   row = @list.size / COLUMN
   row += 1 unless (@list.size % COLUMN).zero?
-  (0..row - 1).each do |x|
-    (0..COLUMN - 1).each do |y|
+  (0...row).each do |x|
+    (0...COLUMN).each do |y|
       print @list[x + row * y].to_s.ljust(24)
     end
     print "\n"
